94c13ee3f98058f283ccbf0dac68296fe823d3e4
y.kagawa <y.kagawa+1@diamondhead.tech>
2022-02-02 10:25:59 +0900

    update: ルート配下にアクセスできるように

--- django/config/urls.py
+++ django/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+import game.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('', game.views.IndexView.as_view()),
 ]
