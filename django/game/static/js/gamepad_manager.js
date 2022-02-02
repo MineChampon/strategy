c131a08038398e84ebd730a07a2160d53595ddf7
y.kagawa <y.kagawa+1@diamondhead.tech>
2022-02-02 18:27:22 +0900

    update: コントローラー接続、もろもろ

--- django/game/static/js/gamepad_manager.js
+++ django/game/static/js/gamepad_manager.js
@@ -1,3 +1,52 @@
-$(function () {
+const CROSS_KEY_UP_DOWN = 5;
+const CROSS_KEY_LEFT_RIGHT = 4;
 
+const BUTTON_A = 1;
+const BUTTON_B = 0;
+
+let gamepad_focus = [4, 4]
+
+// 要ブロッキング
+let map_max_size = 9;
+let map_min_size = 9;
+
+$(function () {
+    window.addEventListener("gamepadconnected", function (e) {
+        let cross_key_up_down_pressed = false;
+        let cross_key_left_right_pressed = false;
+        let button_a_pressed = false;
+        let button_b_pressed = false;
+        setInterval(() => {
+            let gamepad = navigator.getGamepads()[e.gamepad.index];
+
+            if (!(gamepad.buttons[BUTTON_A].pressed)) {
+                button_a_pressed = false;
+            } else if (!(button_a_pressed)) {
+                console.log('Aボタン押下');
+                button_a_pressed = true;
+            };
+
+            if (!(gamepad.buttons[BUTTON_B].pressed)) {
+                button_b_pressed = false;
+            } else if (!(button_b_pressed)) {
+                console.log('Bボタン押下');
+                button_b_pressed = true;
+            };
+
+            if (gamepad.axes[CROSS_KEY_UP_DOWN] == 0) {
+                cross_key_up_down_pressed = false;
+            } else if (!(cross_key_up_down_pressed)) {
+                cross_key_up_down_pressed = true;
+                gamepad_focus[0] += gamepad.axes[CROSS_KEY_UP_DOWN];
+            };
+
+            if (gamepad.axes[CROSS_KEY_LEFT_RIGHT] == 0) {
+                cross_key_left_right_pressed = false;
+            } else if (!(cross_key_left_right_pressed)) {
+                cross_key_left_right_pressed = true;
+                gamepad_focus[1] += gamepad.axes[CROSS_KEY_LEFT_RIGHT];
+            };
+            console.log(gamepad_focus);
+        }, 1000 / 30);
+    });
 });
--- django/game/static/js/map_manager.js
+++ django/game/static/js/map_manager.js
@@ -1,3 +1,29 @@
+const map_over_all = [
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {},],
+];
+
+square_types = [
+    'grass',
+    // 'ocean',
+    'mountain',
+];
+
+const random_map_square_type = () => {
+    if (Math.floor(Math.random() * 2)) {
+        return 'grass'
+    };
+    return square_types[Math.floor(Math.random() * square_types.length)];
+
+};
+
 const add_row_html = (squares_html) => {
     return `
     <div class="map-row w-100">
@@ -10,41 +36,38 @@ const create_map_square_html = (map_square_info) => {
     const map_square_html = `
         <div class="square"
             data-row="${map_square_info['row']}" 
-            data-col="${map_square_info['col']}">
-                座標 => ${map_square_info['row']} : ${map_square_info['col']} 
+            data-col="${map_square_info['col']}"
+            data-type="${map_square_info['type']}"
+        >
+            ${map_square_info['row']} : ${map_square_info['col']} 
         </div>
     `;
     return map_square_html
 };
 
-
-const map_over_all = [
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {},],
-];
-
-
 $(function () {
     const draw_map = () => {
+        $('.map-set-container').html('');
         for (let [row_index, map_row] of map_over_all.entries()) {
             let squares_html_list = [];
             for (let [col_index, map_square] of map_row.entries()) {
                 map_square['row'] = row_index;
                 map_square['col'] = col_index;
+                map_square['type'] = random_map_square_type();
                 squares_html_list.push(
                     create_map_square_html(map_square));
             };
             $('.map-set-container').append(
                 add_row_html(squares_html_list.join('')));
         };
+
+        $('.square').each(function () {
+            square_type = $(this).data('type');
+            $(this).css('background-image', `url("static/image/${square_type}.png")`);
+            if ($(this).data('row') == '4' && $(this).data('col') == '4') {
+                $(this).attr('data-focus', 'True');
+            };
+        });
     };
 
     draw_map();
